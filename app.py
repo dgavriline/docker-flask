cf4e09daa50c3342472e83dde08539f3fbdbf596
dgavriline <62157216+dgavriline@users.noreply.github.com>
2020-05-13 20:54:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return "Hello Alex"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+#from flask import Flask
+#
+#app = Flask(__name__)
+#
+#@app.route('/')
+#def index():
+#    return "Hello Alex"
+#
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=80)
+print("Hello World")
